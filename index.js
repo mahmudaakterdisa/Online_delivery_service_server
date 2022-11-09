7a61b14f8257b27f784c3ed67a546e592e7d18d5
MahmudaDisa <dishamirza.h@gmail.com>
2022-11-09 16:21:46 +0100

    few updates

--- index.js
+++ index.js
@@ -85,6 +85,14 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await reviewCollection.deleteOne(query);
             res.send(result);
+        });
+
+        //update
+
+        app.patch('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status
+
         })
 
     }
