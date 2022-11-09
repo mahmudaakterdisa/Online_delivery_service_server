fe486c81a420e00611de05543136984dd4df865c
MahmudaDisa <dishamirza.h@gmail.com>
2022-11-09 05:54:04 +0100

    post method done

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// const corsOption = { origin: "*", method: ["GET", "POST", "DELETE", "PUT", "PATCH"] }
 //middle wares
 
 app.use(cors());
 app.use(express.json());
+
 
 
 //mongo user setup
@@ -22,6 +24,7 @@ async function run() {
     try {
 
         const serviceCollection = client.db('sweet').collection('services');
+        const reviewCollection = client.db('sweet').collection('review');
 
 
         app.get('/services', async (req, res) => {
@@ -41,6 +44,22 @@ async function run() {
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        //get individual data using id
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        //post review
+        app.post('/review', async (req, res) => {
+
+            const reviews = req.body;
+            const result = await reviewCollection.insertOne(reviews);
+            res.send(result);
         })
 
     }
